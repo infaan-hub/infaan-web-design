a61cae9804d73c5269c48922236849246d818388
infaan-hub <salehabdulrahim8587@gmail.com>
2026-05-08 22:56:15 +0300

    Switch deployment defaults to Fly.io

--- build.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-cd Backend
-chmod +x build.sh
-./build.sh
--- start.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-set -o errexit
-
-cd Backend
-gunicorn infaan_backend.wsgi:application --bind 0.0.0.0:${PORT:-10000}
